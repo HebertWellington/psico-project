9a69a0d2b2fd2a13b9fe25efdbea5b4c269e1a18
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-06-09 10:54:23 -0300

    Initial commit

---
+++ src/main/java/com/psicoproject/PsicoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.psicoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psicoproject/PsicoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psicoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
