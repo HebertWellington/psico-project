d2c2c562edf54b849ee9bbca1b234f30f2fc819e
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-06-14 13:21:28 -0300

    Create UserSaveDto.java

---
+++ src/main/java/com/psicoproject/dto/UserSaveDto.java
@@ -0,0 +1,45 @@
+package com.psicoproject.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.psicoproject.domain.Client;
+import com.psicoproject.domain.User;
+import com.psicoproject.domain.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserSaveDto {
+	
+	@NotBlank(message = "Name required!")	
+	private String username;
+	
+	@Size(min = 7, max = 99, message = "Password between 7 and 99 characters")
+	private String password;
+	
+	@NotBlank(message = "Email required!")
+	@Email(message = "Email invalid!")
+	private String email;
+	
+	
+	@NotNull(message = "Role required!")
+	private Role role;
+	
+	private List<Client> clients = new ArrayList<Client>();
+	
+	public User transformDto() {
+		User user = new User(null, this.username, this.password, this.email, this.role, this.clients);
+		return user;
+		
+	}
+}
