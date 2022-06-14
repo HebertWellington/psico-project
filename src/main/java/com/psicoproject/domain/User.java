16b27d4bfd3f0bfa2614099a21a6d144c0daca92
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-06-14 13:21:13 -0300

    Create User.java

---
+++ src/main/java/com/psicoproject/domain/User.java
@@ -0,0 +1,55 @@
+package com.psicoproject.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.psicoproject.domain.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -4784917572248808526L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 75, nullable = false)
+	private String username;
+	
+	@Getter(onMethod = @__({@JsonIgnore}))
+	@Setter(onMethod = @__({@JsonProperty}))
+	@Column(length = 100, nullable = false)
+	private String password;
+
+	@Column(length = 75, nullable = false, unique = true)
+	private String email;
+
+	@Column(length = 20, nullable = false, updatable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	@Getter(onMethod = @__({@JsonIgnore}))
+	@OneToMany(mappedBy = "user")
+	private List<Client> clients;
+}
