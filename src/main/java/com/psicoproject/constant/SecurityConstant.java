5b3864accc3e9d96864e2591ac8d4ea45932124b
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-06-29 18:37:14 -0300

    Create SecurityConstant.java

---
+++ src/main/java/com/psicoproject/constant/SecurityConstant.java
@@ -0,0 +1,10 @@
+package com.psicoproject.constant;
+
+public class SecurityConstant {
+	
+	public static final int JWT_EXP_DAYS = 5;
+	public static final String API_KEYS = "psicoproject";
+	public static final String JTW_PROVIDER = "Bearer";
+	public static final String JWT_ROLE_KEY = "Role";
+	public static final String JWT_INVALID_MSG = "Invalid Jwt token";
+}
