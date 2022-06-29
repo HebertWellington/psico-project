62e6dc4306c9823033f081a472c022366ad6427a
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-06-29 18:37:50 -0300

    Update UserResource.java

--- src/main/java/com/psicoproject/resource/UserResource.java
+++ src/main/java/com/psicoproject/resource/UserResource.java
@@ -1,14 +1,22 @@
 package com.psicoproject.resource;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,9 +28,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.psicoproject.domain.Client;
 import com.psicoproject.domain.User;
 import com.psicoproject.dto.UserLoginDto;
+import com.psicoproject.dto.UserLoginResponseDto;
 import com.psicoproject.dto.UserSaveDto;
+import com.psicoproject.dto.UserUpdateDto;
+import com.psicoproject.dto.UserUpdateRoleDto;
 import com.psicoproject.model.PageModel;
 import com.psicoproject.model.PageRequestModel;
+import com.psicoproject.security.AccessManager;
+import com.psicoproject.security.JwtManager;
 import com.psicoproject.service.ClientService;
 import com.psicoproject.service.UserService;
 
@@ -31,11 +44,21 @@ import com.psicoproject.service.UserService;
 public class UserResource {
 	
 	@Autowired
-	UserService userService;
+	private UserService userService;
 	
 	@Autowired
-	ClientService clientService;
+	private ClientService clientService;
 	
+	@Autowired
+	private AuthenticationManager authManager;
+	
+	@Autowired
+	private JwtManager jwtManager;
+	
+	@Autowired
+	private AccessManager accessManager;
+	
+	@Secured("ROLE_ADMINISTRATOR")
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody @Valid UserSaveDto userDto){
 		User user = userDto.transformDto();
@@ -43,14 +66,16 @@ public class UserResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 	}
 	
+	@PreAuthorize("@accessManager.isUser(#id)")
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<User> update(@PathVariable(value = "id") Long id, @RequestBody @Valid UserSaveDto userDto){
+	public ResponseEntity<User> update(@PathVariable(value = "id") Long id, @RequestBody @Valid UserUpdateDto userDto){
 		User user = userDto.transformDto();
 		user.setId(id);
 		User userCreated = userService.update(user);
 		return ResponseEntity.ok(userCreated);
 	}
 	
+	@Secured("ROLE_ADMINISTRATOR")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> getById(@PathVariable(value = "id") Long id){
 		User userId = userService.getById(id);
@@ -58,11 +83,23 @@ public class UserResource {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<User> login(@RequestBody @Valid UserLoginDto userDto){
-		User user = userService.login(userDto.getEmail(), userDto.getPassword());
-		return ResponseEntity.ok(user);
+	public ResponseEntity<UserLoginResponseDto> login(@RequestBody @Valid UserLoginDto userDto){
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDto.getEmail(), userDto.getPassword());
+		Authentication auth = authManager.authenticate(token);
+		
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		
+		org.springframework.security.core.userdetails.User userDetail = (org.springframework.security.core.userdetails.User) auth.getPrincipal();
+		
+		String email = userDetail.getUsername();
+		List<String> role = auth.getAuthorities().stream().map(authorit -> authorit.getAuthority()).collect(Collectors.toList());
+		
+		UserLoginResponseDto jwt = jwtManager.createToken(email, role);
+		
+		return ResponseEntity.ok(jwt);
 	}
 	
+	@Secured("ROLE_ADMINISTRATOR")
 	@GetMapping
 	public ResponseEntity<PageModel<User>> listAll(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "10")int size){
 		 PageRequestModel pr = new PageRequestModel(page, size);
@@ -70,10 +107,21 @@ public class UserResource {
 		 return ResponseEntity.ok(pm);
 	}
 	
+	@PreAuthorize("@accessManager.isUser(#id)")
 	@GetMapping("/{id}/clients")
 	public ResponseEntity<PageModel<Client>> listAllByUserId(@PathVariable(value = "id") Long id, @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "10")int size){
 		PageRequestModel pr = new PageRequestModel(page, size);
 		PageModel<Client> pm = clientService.listAllByUserIdOnLazyMode(id, pr);
 		return ResponseEntity.ok(pm);
+	}
+	
+	@Secured("ROLE_ADMINISTRATOR")
+	@PatchMapping("/role/{id}")
+	public ResponseEntity<?> updateRole(@PathVariable(value = "id") Long id, @RequestBody @Valid UserUpdateRoleDto roleDto){
+		User user = new User();
+		user.setId(id);
+		user.setRole(roleDto.getRole());
+		userService.updateRole(user);
+		return ResponseEntity.ok().build();
 	}
 }
