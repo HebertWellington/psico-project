a1caee4f8c6db0dea47cfb25025bee4a64d5f9b2
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-07-05 11:25:50 -0300

    Update ClientRepositoryTest.java

--- src/test/java/com/psicoproject/repository/ClientRepositoryTest.java
+++ src/test/java/com/psicoproject/repository/ClientRepositoryTest.java
@@ -6,7 +6,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,62 +20,92 @@ import com.psicoproject.domain.Client;
 import com.psicoproject.domain.User;
 import com.psicoproject.domain.enums.Role;
 
+@TestMethodOrder(OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class ClientRepositoryTest {
 
 	@Autowired
-	UserRepository userRepository;
-	
-	@Autowired
-	ClientRepository clientRepository;
+	private UserRepository userRepository;
 
-	@Test
-	public void saveTest() {
-		User user = new User(null, "Hebert", "1234567", "hebertwellington@gmail.com", Role.ADMINISTRATOR, null);
+	@Autowired
+	private ClientRepository clientRepository;
+
+	private User user;
+
+	private Client client;
+
+	private Long list;
+
+	private List<Client> listUp;
+
+	@BeforeAll
+	public void setup() {
+		Long list = clientRepository.count();
+		this.list = list;
+		
+		User user = new User(null, "Teste", "1234567", "teste@gmail.com", Role.ADMINISTRATOR, null);
 		userRepository.save(user);
+		this.user = user;
 		
-		Client client = new Client(null, new Date(), "Douglas", 35, "Developer Java", "doug@gmail.com", "1198435500", "08/08/1987", "321.234.321.01", user);  
+		List<Client> listUserId = clientRepository.findAllByUserId(user.getId());
+		this.listUp = listUserId;
+		
+	}
+	@Order(1)
+	@Test
+	public void salvarNovoClienteNoBancoDeDados() {
+		Client client = new Client(null, new Date(), "ClientTest", 35, "Developer Java", "clienttest@gmail.com", "1198435500",
+				"08/08/1987", "321.234.321.01", user);
 		Client clientCreated = clientRepository.save(client);
+		this.client = clientCreated;
 		
-		assertThat(clientCreated.getId()).isEqualTo(1L);
+		assertThat(clientCreated.getCpf()).isEqualTo("321.234.321.01");
 
 	}
 
+	@Order(2)
 	@Test
-	public void updateClientTest() {
-		Optional<Client> result = clientRepository.findById(1L);
+	public void atualizarClienteNBancoDeDados() {
+		Optional<Client> result = clientRepository.findById(client.getId());
 		Client clientUpdate = result.get();
 
-		if(result.isPresent()) {
-			clientUpdate.setName("Douglas Barros");
+		if (result.isPresent()) {
+			clientUpdate.setName("ClientTest Updated");
 			clientUpdate.setCreationDate(new Date());
 			clientRepository.save(clientUpdate);
 		}
-		
-		assertThat(clientUpdate.getName()).isEqualTo("Douglas Barros");
- 	}
-	
+
+		assertThat(clientUpdate.getName()).isEqualTo("ClientTest Updated");
+	}
+
 	@Test
-	public void getByIdClientTest() {
-		Optional<Client> result = clientRepository.findById(1L);
+	public void deveRetornarClientePorIdNoBancoDeDados() {
+		Optional<Client> result = clientRepository.findById(client.getId());
 		Client client = result.get();
-		
+
 		assertThat(client.getAge()).isEqualTo(35);
 	}
-	
+
 	@Test
-	public void listAllClientTest() {
-		List<Client> list = clientRepository.findAll();
-		
-		assertThat(list.size()).isEqualTo(1);
-		
+	public void deveRetornarListaClientsDoBancoDeDados() {
+		Long listUpdate = clientRepository.count();
+
+		assertThat(listUpdate).isEqualTo(list + 1L);
+
+	}
+
+	@Test
+	public void deveRetornarListaClientesPorUserIdDoBancoDeDados() {
+		List<Client> listUpdate = clientRepository.findAllByUserId(user.getId());
+
+		assertThat(listUpdate.size()).isEqualTo(listUp.size() + 1);
+
 	}
 	
-	@Test
-	public void listByUserIdTest() {
-		List<Client> list = clientRepository.findAllByUserId(1L);
-		
-		assertThat(list.size()).isEqualTo(1);
-
+	@AfterAll
+	public void deleteRows() {
+		clientRepository.deleteById(client.getId());
+		userRepository.deleteById(user.getId());
 	}
 }
