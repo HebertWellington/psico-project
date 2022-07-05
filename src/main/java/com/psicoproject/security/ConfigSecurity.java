8b85a3fd99ae04ea567387736d54d9f1ac655a87
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-07-05 11:25:44 -0300

    Update ConfigSecurity.java

--- src/main/java/com/psicoproject/security/ConfigSecurity.java
+++ src/main/java/com/psicoproject/security/ConfigSecurity.java
@@ -20,40 +20,36 @@ import com.psicoproject.service.UserService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private CustomPasswordEncoder passwordEncoder;
-	
-		
+
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
-		
+
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-				   .authorizeRequests()
-				   .anyRequest()
-				   .authenticated();
-		
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated();
+
 		http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 	}
-	
+
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
-		
+
 		return super.authenticationManagerBean();
 	}
 }
