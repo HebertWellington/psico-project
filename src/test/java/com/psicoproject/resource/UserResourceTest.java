6a2cf70e7e6d3ab0a985983ceb9e90225ddff128
Hebert Wellington de Barros Silva <hebertwellington@gmail.com>
2022-07-11 11:25:01 -0300

    Update UserResourceTest.java

--- src/test/java/com/psicoproject/resource/UserResourceTest.java
+++ src/test/java/com/psicoproject/resource/UserResourceTest.java
@@ -1,37 +1,46 @@
 package com.psicoproject.resource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.psicoproject.domain.Client;
 import com.psicoproject.domain.User;
 import com.psicoproject.domain.enums.Role;
 import com.psicoproject.dto.UserLoginDto;
 import com.psicoproject.dto.UserSaveDto;
-import com.psicoproject.repository.UserRepository;
+import com.psicoproject.dto.UserUpdateDto;
+import com.psicoproject.security.AccessManager;
 import com.psicoproject.service.UserService;
+import com.psicoproject.service.util.HashUtil;
 
 @TestMethodOrder(OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -52,9 +61,14 @@ public class UserResourceTest {
 	private UserService userService;
 
 	@MockBean
-	private UserRepository userRepository;
+	private AuthenticationManager auth;
 
-	private User userNew;
+	@MockBean
+	private AccessManager accessManager;
+
+	private String token;
+
+	private User user;
 
 	@BeforeAll
 	public void testLocação() {
@@ -62,24 +76,96 @@ public class UserResourceTest {
 	}
 
 	@Test
-	public void criarUsuario() throws JsonProcessingException, Exception {
+	@Order(2)
+	public void deveRetornarStatusCreated() throws JsonProcessingException, Exception {
 		// cenario
 		UserSaveDto userDto = new UserSaveDto("Test", "1234567", "test@gmail.com", Role.ADMINISTRATOR, null);
 		User user = userDto.transformDto();
 		user.setId(1L);
-		this.userNew = user;
-		Optional<User> result = Optional.of(user);
 		Mockito.when(userService.save(user)).thenReturn(user);
-		Mockito.when(userRepository.findByEmail(user.getEmail())).thenReturn(result);
 
 		// acao
 		mockMvc.perform(post("/users").contentType("application/json").content(objectMapper.writeValueAsString(userDto))
-				.header("Authorization",
-						"BearereyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImV4cCI6MTY1NzQ5MTMzOCwiUm9sZSI6WyJST0xFX0FETUlOSVNUUkFUT1IiXX0.uo7xjEVwneKsy8l8nJyT5cOEy36J4MEfuqOhj6rtXmQwVNoIHHjrshZyqMyu-kGJqP3sN2wDux_gXy3o11JjPw"))
+				.header("Authorization", "Bearer" + token))
 
 				// verificacao
 				.andExpect(status().isCreated());
 
+	}
+
+	@Test
+	@Order(1)
+	public void deveRetornarStatusOkComToken() throws JsonProcessingException, Exception {
+		// cenario
+		UserSaveDto userDtoSave = new UserSaveDto("Test", "1234567", "test@gmail.com", Role.ADMINISTRATOR, null);
+		User user = userDtoSave.transformDto();
+		String hash = HashUtil.securedHash(user.getPassword());
+		user.setId(1L);
+		user.setPassword(hash);
+		UserLoginDto userDto = new UserLoginDto("1234567", user.getEmail());
+
+		List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+
+		Mockito.when(auth.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), "1234567"))).thenReturn(
+				new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(
+						user.getEmail(), hash, true, true, true, true, authorities), hash, authorities));
+
+		// acao
+		MvcResult result = mockMvc
+				.perform(post("/users/login").contentType("application/json")
+						.content(objectMapper.writeValueAsString(userDto)))
+
+				// verificacao
+				.andExpect(status().isOk()).andReturn();
+
+		token = JsonPath.read(result.getResponse().getContentAsString(), "$.token");
+
+		assertThat(token).isNotNull();
+
+	}
+
+	@Test
+	public void deveRetornarStatusOkComValidacaoToken() throws JsonProcessingException, Exception {
+		// cenario
+		UserUpdateDto userDto = new UserUpdateDto("Test", "1234567", "testupdate@gmail.com", null);
+		user = userDto.transformDto();
+		user.setId(1L);
+		List<Client> clients = new ArrayList<>();
+		user.setClients(clients);
+		Mockito.when(userService.update(user)).thenReturn(user);
+		Mockito.when(accessManager.isUser(user.getId())).thenReturn(true);
+
+		// acao
+		MvcResult result = mockMvc
+				.perform(put("/users/{id}", 1L).contentType("application/json")
+						.content(objectMapper.writeValueAsString(userDto)).header("Authorization", "Bearer" + token))
+
+				// verificacao
+				.andExpect(status().isOk()).andReturn();
+		String UserUpdate = JsonPath.read(result.getResponse().getContentAsString(), "$.email");
+		assertThat(UserUpdate).isEqualTo("testupdate@gmail.com");
+	}
+
+	@Test
+	public void deveRetornarStatusNaoAutorizado() throws JsonProcessingException, Exception {
+		// cenario
+		UserUpdateDto userDto = new UserUpdateDto("Test", "1234567", "testupdate@gmail.com", null);
+		user = userDto.transformDto();
+		user.setId(1L);
+		List<Client> clients = new ArrayList<>();
+		user.setClients(clients);
+		Mockito.when(userService.update(user)).thenReturn(user);
+		Mockito.when(accessManager.isUser(user.getId())).thenReturn(false);
+
+		// acao
+		MvcResult result = mockMvc
+				.perform(put("/users/{id}", 1L).contentType("application/json")
+						.content(objectMapper.writeValueAsString(userDto)).header("Authorization", "Bearer" + token))
+
+				// verificacao
+				.andExpect(status().isForbidden()).andReturn();
+		String UserUpdate = JsonPath.read(result.getResponse().getContentAsString(), "$.msg");
+		assertThat(UserUpdate).isEqualTo("Access is denied");
 	}
 
 }
